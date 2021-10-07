2bcf30d91ba9e950404a610860347136f97561a8
carsonology <61434744+carsonology@users.noreply.github.com>
2021-10-07 17:37:49 -0400

    upload python file

---
+++ p_tag_generator.py
@@ -0,0 +1,28 @@
+#insert p tags between paragraph breaks
+
+#get paragraphs that will be wrapped in p tags from user
+initial_string = input("Input paragraphs:")
+
+#split grafs at each return
+grafs = initial_string.split("\n")
+
+#get rid of any extra newlines
+[grafs.remove(graf) for graf in grafs if not graf]
+
+#initiate p tag string that will contain wrapped grafs
+p_tag_grafs = "<p>"
+
+#insert p tags between each paragraph
+for index, graf in enumerate(grafs):
+    p_tag_grafs += "\n\t" + graf + "\n</p>"
+    if index != len(grafs) - 1:
+        p_tag_grafs += "\n<p>"
+        
+print(p_tag_grafs)
+
+#input desired csv file name for final output
+file_name = "choc-cookie.txt"
+
+#write p tag grafs to a file ready for copying
+with open(file_name, 'w') as file:
+    file.write(p_tag_grafs)
