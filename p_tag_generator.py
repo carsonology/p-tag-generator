8c5fcf43094ef76a276bd3ccc7627c9067a4602d
carsonology <61434744+carsonology@users.noreply.github.com>
2021-10-15 15:24:00 -0400

    updated to fix input bug

--- p_tag_generator.py
+++ p_tag_generator.py
@@ -1,27 +1,31 @@
 #insert p tags between paragraph breaks
 
 #get paragraphs that will be wrapped in p tags from user
-initial_string = input("Input paragraphs:")
-
-#split grafs at each return
-grafs = initial_string.split("\n")
-
-#get rid of any extra newlines
-[grafs.remove(graf) for graf in grafs if not graf]
+# note: you must type the word "end" in all lowercase after all the lines
+# you want to input so the program knows when to stop collecting lines.
+lines = []
+print('Copy and paste paragraphs, with the word "end" as the final paragraph: ')
+while True:
+    line = input()
+    if line != "end":
+        if line:
+            lines.append(line)
+    else:
+        break
 
 #initiate p tag string that will contain wrapped grafs
 p_tag_grafs = "<p>"
 
 #insert p tags between each paragraph
-for index, graf in enumerate(grafs):
+for index, graf in enumerate(lines):
     p_tag_grafs += "\n\t" + graf + "\n</p>"
-    if index != len(grafs) - 1:
+    if index != len(lines) - 1:
         p_tag_grafs += "\n<p>"
-        
+
 print(p_tag_grafs)
 
 #input desired csv file name for final output
-file_name = "your-file-name.txt"
+file_name = "file-name.txt"
 
 #write p tag grafs to a file ready for copying
 with open(file_name, 'w') as file:
